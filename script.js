b06267e8667472d96c881e9c06ee9f38f36f2e20
Meghabhatt23 <44779627+Meghabhatt23@users.noreply.github.com>
2020-01-28 12:19:18 -0700

    correct code

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-enterDays.onclick = calculate;
-function calculate(){
+function Calculate(){
     var amount = document.getElementById("amount");
     var apr = document.getElementById("apr");
     var years = document.getElementById("years");
-    var zipcode = document.getElementById("zipcode");
-    var payment = document.getElementById("payment");
     var total = document.getElementById("total");
 
     var principal = parseFloat(amount.value);
@@ -12,8 +9,6 @@ function calculate(){
     var InterestDividesMonth= parseFloat(apr.value)/100;
 
     var onePlusInterestDividesMonthSquare_By_monthTimesYear= Math.pow(1+InterestDividesMonth, monthTimesYear);
-
     var final = principal*onePlusInterestDividesMonthSquare_By_monthTimesYear;
-
     total.innerHTML="$" + final.toFixed(2);
 }
